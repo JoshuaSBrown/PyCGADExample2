c5c988d2aa9019a26c9fb88c158dfd37df1852de
JoshuaSBrown <joshbro42867@yahoo.com>
2021-06-11 17:27:14 -0600

    Begin testing methods

--- tests/test_upload_download.py
+++ tests/test_upload_download.py
@@ -10,17 +10,37 @@ def test_app():
 
     current_path, _ = os.path.split(os.path.abspath(__file__))
     repo_path = os.path.normpath(os.path.join(current_path, "../"))
-    pem_file_path = os.path.join(
-        repo_path, "statusreportingapp.2021-05-29.private-key.pem"
-    )
+    for file_name in os.listdir(repo_path):
+        if file_name.lower().endswith(".pem"):
+            print("Found pem file {}".format(file_name))
+            pem_file_path = os.path.join(repo_path, file_name)
+            break
+
     app.initialize(pem_file=pem_file_path, path_to_repo=app.generateCandidateRepoPath())
 
     return app
 
 
-def test_download(test_app):
-   print("Download file") 
+def test_get_branch_tree(test_app):
+   branch_tree = test_app.getBranchTree("master")
+   print(branch_tree)
+
+   assert branch_tree.exist("bin")
+   assert branch_tree.exist("README.md")
+
+   assert branch_tree.type("bin") == "dir"
+   assert branch_tree.type("README.md") == "file"
+
+   rel_paths = branch_tree.getRelativePaths("test_upload_download.py")
+   assert len(rel_paths) == 1
+   assert rel_paths[0] == "./tests/test_upload_download.py"
+
+def test_get_contents(test_app):
+   branch_content = test_app.getContents("master")
+
+   print(branch_content)
+
+   assert branch_content.exist("bin")
+   assert branch_content.exist("README.md")
 
 
-def test_upload(test_app):
-    print("Upload file")
--- upload_download/upload_download_app.py
+++ upload_download/upload_download_app.py
@@ -3,11 +3,11 @@ import os
 
 class UploadDownloadApp(GitHubApp):
     def __init__(self, verbosity_in = 0):
-        """Upload Download app can upload and donwload files from  repo"""
+        """Upload Download app can upload and download files from repo and its wiki"""
         if isinstance(verbosity_in, list):
             verbosity_in = verbosity_in[0]
         super().__init__(
-            117711,
+            120492,
             "UploadDownloadApp",
             "JoshuaSBrown",
             "PyCGADExample2",
